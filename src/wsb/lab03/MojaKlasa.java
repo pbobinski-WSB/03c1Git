1cf8a9d96573572001553a130390f2aa25fa8615
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2021-10-31 19:22:33 +0100

    pierwszy commit

---
+++ src/wsb/lab03/MojaKlasa.java
@@ -0,0 +1,13 @@
+package wsb.lab03;
+
+public class MojaKlasa {
+
+    private int poleLiczbowe = 12;
+
+    @Override
+    public String toString() {
+        return "MojaKlasa{" +
+                "poleLiczbowe=" + poleLiczbowe +
+                '}';
+    }
+}
---
+++ src/wsb/lab03/Test.java
@@ -0,0 +1,12 @@
+package wsb.lab03;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        MojaKlasa mk = new MojaKlasa();
+
+        System.out.println(mk);
+
+    }
+}
